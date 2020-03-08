0f48b09164938c4cfc7ef4c5eb740a7621b6f65d
Md. Rakibul Hasan <soundmind1992@gmail.com>
2020-03-08 13:36:24 +0600

    null vs undefined cleared for interview

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,18 +1,18 @@
-//let pakhi;
-//console.log(pakhi);
+let pakhi;
+console.log(pakhi);
 
-// function add (num1, num2){
-//     console.log(num1+num2);
-//     return 
-// }
-// const result = add(13, 82);
-// console.log(result);
+function add (num1, num2){
+    console.log(num1+num2);
+    return 
+}
+const result = add(13, 82);
+console.log(result);
 
-// function add(num1, num2){
-// console.log(num1, num2);
-// }
-// const result = add(13,);
-// console.log(result);
+function add(num1, num2){
+console.log(num1, num2);
+}
+const result = add(13,);
+console.log(result);
 
 const premik = {name: "smart dude", phone: 458421};
 console.log(premik.gf);
