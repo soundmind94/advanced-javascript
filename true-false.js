9fb987e652870d5fc21c0bb9d9bcf4650b5f195f
Md. Rakibul Hasan <soundmind1992@gmail.com>
2020-03-08 12:32:17 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,21 @@
+
+//Falsy;
+// 0
+// ""
+//undefined
+//null
+//NaN
+//false
+
+//Truthy;
+//'0', "",'',[],{},'false'
+
+
+
+let name = 12;
+if (name || name == 0){
+    console.log("condition is true");
+} 
+else{
+    console.log("condition is false")
+}
